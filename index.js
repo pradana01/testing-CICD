c515c2f4043d0c793226cad187b6131d52c49ab5
pradana01 <andreanwirapradana@gmail.com>
2023-07-12 09:42:08 +0700

    First push

---
+++ index.js
@@ -0,0 +1,19 @@
+function calculator(n1, n2, operator) {
+    switch (operator) {
+        case "+":
+            return n1 + n2;
+        case "-":
+            return n1 - n2;
+        case "*":
+            return n1 * n2;
+        case "/":
+            return n1 / n2;
+        case "%":
+            return n1 % n2;
+    
+        default:
+            return "Please use +, -, *, /, %"
+    }
+}
+
+console.log(calculator(5,5,"%"))
